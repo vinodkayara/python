55a8e816d3b30141b77173875f2c613b19b7081e
Vinod Kayara <115912502+vinodkayara@users.noreply.github.com>
2023-08-23 09:33:22 +0530

    Add files via upload

---
+++ 4b.py
@@ -0,0 +1,20 @@
+def romanToDec(romStr):
+    roman_dict ={'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    # Analyze string backwards
+    romanBack = list(romStr)[::-1]
+    value = 0
+    # To keep track of order
+    rightVal = roman_dict[romanBack[0]]
+    for numeral in romanBack:
+        leftVal = roman_dict[numeral]
+        # Check for subtraction
+        if leftVal < rightVal:
+           value -= leftVal
+        else:
+            value += leftVal
+        rightVal = leftVal
+    return value
+
+
+romanStr = input("Enter a Roman Number : ")
+print(romanToDec(romanStr))
