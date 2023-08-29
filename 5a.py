1ab5edadca055980b815b2149908daef002eb06f
Vinod Kayara <115912502+vinodkayara@users.noreply.github.com>
2023-08-29 09:28:35 +0530

    Add files via upload

---
+++ 5a.py
@@ -0,0 +1,10 @@
+import re
+def isphonenumber(numStr):
+    if len(numStr)!=12:
+        return False
+    for i in range(len(numStr)):
+        if i=3 or i=7:
+            if numStr[i]="-":
+                return False
+            else:
+                if numbeStr[i].i digit
